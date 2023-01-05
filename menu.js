8a88732fa5c6bf6d5507dec069e6eb1cbf47c783
Romain Guilbeau <guilbeau.r@gmail.com>
2023-01-05 20:04:49 +0100

    maj doc

--- menu.js
+++ menu.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
                 {
                     name : "Accueil",
                     description: "Documentation technique du fonctionnement de la reproduction du cockpit A320",
-                    link : "index.html"
+                    link : "../home/home.html"
                 },
                 {
                     name : "Gestion de la documentation",
                     description: "Aide mémoire pour maintenir la documentation",
-                    link : "doc.html"
+                    link : "../doc/doc.html"
                 }
             ]
         },
@@ -19,19 +19,24 @@ $(document).ready(function(){
             title: "CAN Bus",
             items : [
                 {
-                    name : "Configuration / message",
-                    description: "Présentation de la configuration et de la définition des messages du CAN Bus",
-                    link : "canbus.html"
+                    name : "Introduction / drivers",
+                    description: "Vue rapide de l'intégration du CAN Bus dans le cockpit",
+                    link: "../canbus-introduction/canbus-introduction.html"
+                },
+                {
+                    name : "Messages",
+                    description: "Présentation de la définition des messages du CAN Bus",
+                    link : "../canbus-frames/canbus-frames.html"
                 }
             ]
         }
     ];
 
-    var currentPage = document.location.href.substring(document.location.href.lastIndexOf('/') + 1)
+    var currentPage = getLastValueOfHtmlPath(document.location.href);
     var html = '<ul class="list-unstyled ps-0">';
     for(var idxMenu = 0; idxMenu < menu.length; idxMenu++) {
         html += '<li class="mb-1">';
-        html += '   <button class="btn btn-toggle align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#menu-collapse-'+idxMenu+'" aria-expanded="true">';
+        html += '   <button id="menu-collapse-button-'+idxMenu+'" class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#menu-collapse-'+idxMenu+'" aria-expanded="false">';
         html += menu[idxMenu].title;
         html += '   </button>';
         html += '   <div class="collapse" id="menu-collapse-'+idxMenu+'">';
@@ -41,7 +46,7 @@ $(document).ready(function(){
             var item = menu[idxMenu].items[idxItem];
 
             var linkDisplay = 'fw-bolder';
-            if(currentPage !== item.link) {
+            if(currentPage !== getLastValueOfHtmlPath(item.link)) {
                 linkDisplay = 'link-dark';
             }
 
@@ -59,13 +64,17 @@ $(document).ready(function(){
         for(var idxItem = 0; idxItem < menu[idxMenu].items.length; idxItem++) {
             var item = menu[idxMenu].items[idxItem];
             
-            if(currentPage === item.link) {
+            if(currentPage === getLastValueOfHtmlPath(item.link)) {
                 $('title').html('A320 Cockpit - ' + item.name);
                 $('#title').html(menu[idxMenu].title + ' - ' + item.name);
                 $('#description').html(item.description);
                 $('#menu-collapse-' + idxMenu).toggleClass('show');
+                $('#menu-collapse-button-'+idxMenu+'').attr("aria-expanded","true");
             }
         }
     }
 });
 
+function getLastValueOfHtmlPath(path) {
+    return path.substring(path.lastIndexOf('/') + 1);
+}
